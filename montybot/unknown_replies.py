ba59ad427fe30f61bddaf0e4309e645634764126
Esther <esther@pyladies.com>
2014-01-24 09:32:39 -0800

    different unknown-reply

--- montybot/unknown_replies.py
+++ montybot/unknown_replies.py
@@ -1,11 +1,12 @@
 import random
 
 video = 'https://www.youtube.com/watch?v=zGxwbhkDjZM'
+puppy_fail = 'http://gifs.gifbin.com/032013/1363630355_puppy_catch_fail.gif'
 
 SMARTASS_REPLY_LIST = [
     'No comprendo',
     "You're gonna have to make a pull request for that",
-    'Quoi?!',
+    '<-- %s' % (puppy_fail,),
     'What the *what*?',
     "Ain't nobody got time for dat! %s" % video
 ]
